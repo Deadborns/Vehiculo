2d92b8af0d4f4b4cbe0fbd30bb9e999966b7d13d
Deadborns <34080789+Deadborns@users.noreply.github.com>
2019-04-29 08:59:29 -0500

    Commit Aula

--- src/Carro.java
+++ src/Carro.java
@@ -9,5 +9,18 @@
  * @author edromerom
  */
 public class Carro {
+    private String placa;
+    private String marca;
+    private Eje[] eje;
+    private Llanta[] llanta;
+    private Motor motor;
     
+
+    public Carro(String placa, String marca, Eje[] eje, Llanta[] llanta, Motor motor) {
+        this.placa =  placa;
+        this.marca =  marca;
+        this.eje = new Eje[2];
+        this.llanta = new Llanta[4];
+        this.motor =  new Motor();
+    }
 }
